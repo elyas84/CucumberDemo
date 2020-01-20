76a1412b64302a3c789b36b032724a7e5a033dbd
Elyas Arkin <(elyas.arkin@hotmail.se)>
2020-01-20 18:09:06 +0100

    corrected

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 
         //where our feature files are located
         // We whether we can write by hand this location or just click the copyPath
-        plugin = {"json:target/cucumber.json"},
+       plugin = {"json:target/cucumber.json"},
         features = "src/test/resources/features/",
         glue = "com/vytrack/step_definitions" ,
         dryRun =  false ,
